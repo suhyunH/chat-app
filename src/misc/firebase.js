60ca573ce9a865372d5a0e001248f75d8175e781
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-01-13 18:13:22 +0900

    initialize firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const  firebaseConfig = {
+    apiKey: "AIzaSyAY0Wqb5y3sFof1n3SCMtYO9iIa5wqXj2w",
+    authDomain: "cheez-chat-108a3.firebaseapp.com",
+    databaseURL: "https://cheez-chat-108a3-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "cheez-chat-108a3",
+    storageBucket: "cheez-chat-108a3.appspot.com",
+    messagingSenderId: "951633747371",
+    appId: "1:951633747371:web:606891a8f1a58c8fd086ee"
+  };
+
+  const config = initializeApp(firebaseConfig);
