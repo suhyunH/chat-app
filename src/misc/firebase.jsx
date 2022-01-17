556e67d2bd6d9fef50e542891d8a7a1d6b6a8f39
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-01-17 20:28:15 +0900

    sign in page with google & github

--- src/misc/firebase.jsx
+++ src/misc/firebase.jsx
@@ -1,12 +1,21 @@
 import { initializeApp } from "firebase/app";
+import{getAuth} from "firebase/auth";
+import{getDatabase} from "firebase/database";
+// import {getFirestore} from"firebase/firestore";
+// import{getStorage} from"firebase/storage";
+
 const  firebaseConfig = {
-    apiKey: "AIzaSyAY0Wqb5y3sFof1n3SCMtYO9iIa5wqXj2w",
-    authDomain: "cheez-chat-108a3.firebaseapp.com",
-    databaseURL: "https://cheez-chat-108a3-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "cheez-chat-108a3",
-    storageBucket: "cheez-chat-108a3.appspot.com",
-    messagingSenderId: "951633747371",
-    appId: "1:951633747371:web:606891a8f1a58c8fd086ee"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
-  const config = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export const database = getDatabase(app);
+  // export const dbService = getFirestore();
+  // export const storageService = getStorage();
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import { auth, database} from '../misc/firebase'
+import { getAdditionalUserInfo, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { ref, serverTimestamp, set } from 'firebase/database'
 
 const SignIn=()=> {
+    const signInWithProvider = async provider=>{
+        try{
+            const credential= await signInWithPopup(auth, provider);
+            const userMeta = getAdditionalUserInfo(credential);
+
+            if(userMeta.isNewUser){
+                await set(ref(database, `/profiles/${credential.user.uid}`),{
+                    name : credential.user.displayName,
+                    createdAt: serverTimestamp(),
+                });
+            }
+            console.log(userMeta);
+            alert("signed in");
+        }catch{
+           alert("error");
+        }
+    }
+
+    const onGithubSignIn = ()=>{
+        signInWithProvider(new GithubAuthProvider()); 
+    }
+    const onGoogleSignIn = ()=>{
+        signInWithProvider (new GoogleAuthProvider());
+    }
     return (
+        <>
         <div>
-            sign in
+            <h2>Welcome to chat</h2>
+            <p>adklajdklasdj</p>
         </div>
+        <div>
+            <button onClick={onGithubSignIn}>Continue with github</button>
+            <button onClick={onGoogleSignIn}>Continue with google</button>
+        </div>
+        </>
+        
     )
 }
 
